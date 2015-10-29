70dd8f061aefae012ed4e3a2f028ff89a5c9b8ad
zna-com <znacloud@126.com>
2015-10-29 10:46:55 +0800

    rename directory

---
+++ app/src/main/java/com/motouch/android/demo/ui/MainActivity.java
@@ -0,0 +1,37 @@
+package com.motouch.android.timepickerdemo.ui;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.motouch.android.demo.R;
+import com.motouch.android.sectorprogressbar.SectorProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private SectorProgressBar mCirclePbr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCirclePbr = (SectorProgressBar) findViewById(R.id.spb_progress);
+//        mCirclePbr.setSectorBackgroundColor(Color.parseColor("#55000000"));
+//        mCirclePbr.setBorderColor(Color.GRAY);
+//        mCirclePbr.setBorderWidth(4);
+//        mCirclePbr.setShowTextPercent(true);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                mCirclePbr.setProgress((mCirclePbr.getProgress() + 2) % 101);
+//                mCirclePbr.setSectorColor(Color.argb(255,(int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255)));
+                handler.postDelayed(this, 200);
+            }
+        });
+
+    }
+
+}
